61e9fd1823385f89a25dac51280d3d318680b5e9
Pierre Bertet <hello@pierre.world>
2018-03-23 15:36:10 +0000

    Fix aragon-version.sh

--- packages/react-scripts/scripts/aragon-version.sh
+++ packages/react-scripts/scripts/aragon-version.sh
@@ -19,6 +19,7 @@ echo ""
 echo "   $next_version"
 echo ""
 
+echo ""
 echo "Apply the new version with npm? (y/n)? "
 read answer
 if echo "$answer" | grep --invert-match -iq "^y"; then
@@ -30,13 +31,15 @@ mkdir .git
 npm version "$next_version"
 rmdir .git
 
+echo ""
 echo "git push && git push --tags? (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y"; then
-  git push "$aragon_git" next-aragon
-  git push "$aragon_git" --tags
+  git push "$ARAGON_GIT" next-aragon
+  git push "$ARAGON_GIT" --tags
 fi
 
+echo ""
 echo "npm publish? (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y"; then
